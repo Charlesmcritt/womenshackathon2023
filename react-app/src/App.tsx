3a5e9314058eda9e32b2b72db364ddbbf0899ef3
Charlesmcritt <130863946+Charlesmcritt@users.noreply.github.com>
2023-04-15 13:49:18 -0500

    First Commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,9 @@
+import ListGroup from "./components/ListGroup"
+
+
+function App(){
+   
+  return <div><ListGroup></ListGroup></div>
+}
+
+export default App; 
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,6 @@
+function ListGroup(){
+    return<h1>ListGroup</h1>;
+
+}
+
+export default ListGroup; 
